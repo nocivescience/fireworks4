2add789807cf5acc00db2286290235a048367f6c
noceive <nocivescience@gmail.com>
2023-02-18 20:56:40 -0300

    hasta aca funciona el giro de las letras

--- projectos.js
+++ projectos.js
@@ -2,8 +2,8 @@ const projectosString='Proyectos';
 const proyectosEl= document.getElementById('proyectos');
 proyectosEl.classList.add('proyectos');
 function separarLetras(){
-    projectosString.split('').map((letra, i) => {
+    projectosString.split('').forEach((letra, i) => {
         proyectosEl.innerHTML += `<div class="letter">${letra}</div>`;
-    });
+    })
 }
 separarLetras();
