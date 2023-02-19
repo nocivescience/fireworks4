4e53dc7e94c197176b4745b21ab23aec197bc879
noceive <nocivescience@gmail.com>
2023-02-18 23:18:51 -0300

    ya esta listo los items

--- projectos.js
+++ projectos.js
@@ -2,7 +2,7 @@ const projectosString='Proyectos';
 const proyectosEl= document.getElementById('proyectos');
 proyectosEl.classList.add('proyectos');
 function separarLetras(){
-    projectosString.split('').forEach((letra, i) => {
+    projectosString.split('').map((letra, i) => {
         proyectosEl.innerHTML += `<div class="letter">${letra}</div>`;
     })
 }
